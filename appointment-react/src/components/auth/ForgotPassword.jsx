eba9ffc4edbbecb31f087952dcd4e24558247fd4
Anuj <anuj.vgithub25@gmail.com>
2025-02-28 17:01:11 +0530

    chanegs for sendgrid keys

--- appointment-react/src/components/auth/ForgotPassword.jsx
+++ appointment-react/src/components/auth/ForgotPassword.jsx
@@ -4,40 +4,49 @@ import {
   } from "react-router-dom";
 import { useState } from 'react';
 const ForgotPassword = () => {
-    const [enterOtp,setEnterOtp]=useState(false)
+
+
+    const [enterOtp,setEnterOtp]=useState(false);
+    const [otp,setOtp]=useState('');
+    const [email,setEmail]=useState('');
+
+    
     const handleSubmit=async ()=>{
         const response=await axios.post('http://localhost:5000/api/auth/send-otp',{
-            email:'anuj@yopmail.com',
+            email
         })
-        if(response.data.message==='OTP sent successfully'){
+        if(response.data){
             setEnterOtp(true)
         }
-        console.log(response.data)
     }
 
     const handleOTPSubmit=async ()=>{
-        const response=await axios.post('http://localhost:5000/api/auth/send-otp',{
-            email:'anuj@yopmail.com',
+        const response=await axios.post('http://localhost:5000/api/auth/verify-otp',{
+            email,
+            otp:Number(otp)
         })
-        if(response.data.message==='OTP sent successfully'){
+        if(response.data){
             setEnterOtp(true)
         }
         console.log(response.data)
-    }
-    if(enterOtp){
-        return (
-            <div>
-                <h1>Enter OTP</h1>
-                <input type="text" placeholder="OTP" />
-                <button onClick={handleOTPSubmit}>Submit</button>
-            </div>          
-        )
     }
     return (
         <div>
             <h1>Forgot Password</h1>
-            <input type="email" placeholder="Email" />
-            <button onClick={handleSubmit}>Submit</button>
+
+            <div>
+                {enterOtp?
+                 <div>
+                        <h1>Enter OTP</h1>
+                        <input value={otp} onChange={(e)=>setOtp(e.target.value)} type="text" placeholder="OTP" />
+                        <button onClick={handleOTPSubmit}>Submit</button>
+                </div>:
+                <div>   <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Email" />
+                        <button onClick={handleSubmit}>Submit</button>
+                </div>}
+            </div>
+
+            
         </div>
     );
 }
